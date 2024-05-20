fd95772f800b06bb28bb4f6ab8353ce95c471bc6
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-20 11:50:08 +0700

    menampilkan ibu memiliki anak

--- Agreggasi/Agreggasi.cpp
+++ Agreggasi/Agreggasi.cpp
@@ -13,5 +13,10 @@ int main()
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Demi");
 
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
+
    
 }
