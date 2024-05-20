14dfa4dd5c77903f1ecef5b22ff834881af51380
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-20 17:04:33 +0700

    membuat construck dan destruck

--- association/association.cpp
+++ association/association.cpp
@@ -7,6 +7,13 @@ class pasien {
 public:
     string nama;
     vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\"ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\"tidak ada\n";
+    }
+
 
 };
 
