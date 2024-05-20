a081c3992aa0d1d50cce118d2129eb39202e2f80
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-20 17:43:42 +0700

    akses public pada sekolah

--- pewaris/pewaris.cpp
+++ pewaris/pewaris.cpp
@@ -21,6 +21,17 @@ public:
 
 class pelajar : public orang {
 public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
 };
 
 int main()
