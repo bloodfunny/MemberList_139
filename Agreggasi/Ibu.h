3b3be2f7d94671a8b27874299ea716f9a4082d56
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-20 11:47:30 +0700

    membuat akses public pada nama dan daftar nama

--- Agreggasi/Ibu.h
+++ Agreggasi/Ibu.h
@@ -1,7 +1,20 @@
 #ifndef Ibu_h
 #define Ibu_h
-#include <vektor>
+#include <vector>
+
+using namespace std;
+#include <iostream>
 
 class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
 
-};
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "Ibu \"" << nama << "\" tidak ada\n";
+	}
+
+	
