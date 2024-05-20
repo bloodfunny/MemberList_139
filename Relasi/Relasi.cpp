5fd16c346371f131d56b8dc5c000f5478ad027ef
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-20 10:30:39 +0700

    membuat class mahasiswa

--- Relasi/Relasi.cpp
+++ Relasi/Relasi.cpp
@@ -3,12 +3,6 @@
 using namespace std;
 
 class mahasiswa {
-private:
-	const int id;
-	string nama;
-	float nilai;
 
-public:
-	mahasiswa (int pId, string pNama, float pNilai) :id(pId),nama(pNama)
 
 };
