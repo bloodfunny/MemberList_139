c24ac5741f03589b9bda14fab0ee05216fa8f67d
bloodfunny <adhyaakmalrafiansyah@email.com>
2024-05-20 17:46:04 +0700

    int main menampilkan siswa

--- pewaris/pewaris.cpp
+++ pewaris/pewaris.cpp
@@ -34,8 +34,11 @@ public:
     }
 };
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
 }
 
